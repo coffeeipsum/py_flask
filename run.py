4c69bf9c6a4fa5c12a0000214531f7edbb56a3d3
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-06-21 12:22:29 +0000

    Base, Careers added

--- run.py
+++ run.py
@@ -7,14 +7,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
-    
-@app.route('/')
+
+
+@app.route('/about')
 def about():
     return render_template("about.html")
-    
-@app.route('/')
+
+
+@app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+
+@app.route('/careers')
+def careers():
+    return render_template("careers.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
